261c63f92529a7a662da0e5bad02357b8a88cc62
Mubashir Ahmed <mdmubashirahmed12345@gmail.com>
2023-11-06 23:36:23 +0530

    try

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import OngoingEvents from "./OngoingEvents";
 import PastEvents from "./PastEvents";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../firebase";
